5284e29a4775137a114b3ee1df80be421d7e7aa8
coherentchaos <scott.axcell@gmail.com>
2019-02-22 19:31:48 -0700

    update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
-CLASSES=<project_directory>
-SCRIPT="cd $CLASSES; java -cp . cs455.scaling.client.Client server-hostname server-port message-frequency"
+CLASSES=`pwd`/out/production/classes
+#CLASSES=`pwd`/build/classes/java/main
+SCRIPT="cd $CLASSES; java -cp . cs455.scaling.client.Client phoenix 50321 2"
 for ((j=1;j<=$1;j++));
 do
     COMMAND='gnome-terminal'
